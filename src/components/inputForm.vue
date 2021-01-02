0f7712d3a392559c62491819c4357926bd4ec5e1
cheoryeon <rlacjfgus2@gmail.com>
2021-01-02 19:01:26 +0900

    InputForm 추가

---
+++ src/components/inputForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="input-form">
+        <input :type="inputType" :id="inputId" :placeholder="inputPlaceHolder">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputForm',
+    props: [
+            'inputType', 
+            'inputId', 
+            'inputPlaceHolder'
+        ]
+}
+</script>
+
+<style>
+
+.input-form > input {
+  width: 100%;
+  background: 0 0;
+  background: #f9f9f9;
+  border: 1px solid #ebebeb;
+  border-radius: 3px;
+  box-sizing: border-box;
+  color: #444444;
+  font-size: 13px;
+  padding: 7px 8px 7px;
+}
+
+</style>
