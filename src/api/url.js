7804727534d79d9b1bd07905a803a87579fb80ac
cheoryeon <rlacjfgus2@gmail.com>
2021-01-10 10:58:53 +0900

    서버 URL  처리

---
+++ src/api/url.js
@@ -0,0 +1,4 @@
+const BASE_URL = "http://localhost:9000/"
+
+
+export {BASE_URL}
--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
-
-const BASE_URL = "http://localhost:9000/"
+import BASE_URL from './url.js'
 
 async function login(id, password) {
     
@@ -9,7 +8,7 @@ async function login(id, password) {
         'userPw' : password
     }
 
-    await axios.post(BASE_URL + 'login', params) 
+    return await axios.post(BASE_URL + 'login', params) 
                                 .catch(function (error) {
                                   if (error.response) {
                                     // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
@@ -27,6 +26,14 @@ async function login(id, password) {
                                     // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
                                     console.log('Error', error.message);
                                   }
+                                })
+                                .then( res => {
+                                    if(res === 'Y') {
+                                      console.log('로그인 성공')
+                                    }
+                                    else {
+                                      console.log('로그인 실패')
+                                    }
                                 });
     
 }
